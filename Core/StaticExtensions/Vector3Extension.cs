3a5f705fb611d741e9f68ff9bfc07bd4a4c1b88c
Kévin Roussel <keeevin.roussel@gmail.com>
2025-04-27 16:01:45 +0200

    Add Extension to Vector2

--- Core/StaticExtensions/Vector3Extension.cs
+++ Core/StaticExtensions/Vector3Extension.cs
@@ -15,6 +15,10 @@ public static class Vector3Extention
         Mathf.Approximately(a.y, b.y) && 
         Mathf.Approximately(a.z, b.z);
 
+    public static bool ApproximatelyEqualTo(this Vector2 a, Vector2 b) =>
+        Mathf.Approximately(a.x, b.x) &&
+        Mathf.Approximately(a.y, b.y);
+
     public static Vector3 SetX(this Vector3 @this, float newValue) => new Vector3(newValue, @this.y, @this.z);
     public static Vector3 SetY(this Vector3 @this, float newValue) => new Vector3(@this.x, newValue, @this.z);
     public static Vector3 SetZ(this Vector3 @this, float newValue) => new Vector3(@this.x, @this.y, newValue);
