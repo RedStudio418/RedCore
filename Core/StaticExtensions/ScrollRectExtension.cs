92e7c5e77ef9eecc09373bafc3069078f7d1b8ee
Kévin Roussel <keeevin.roussel@gmail.com>
2025-10-02 16:49:03 +0200

    ScrollRect Extension improved

--- Core/StaticExtensions/ScrollRectExtension.cs
+++ Core/StaticExtensions/ScrollRectExtension.cs
@@ -1,22 +1,39 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public static class ScrollRectExtension
 {
 
     public static void FocusElement(this ScrollRect @this, GameObject from, GameObject to)
     {
-        RectTransform fromRect = from.GetComponent<RectTransform>();
-        RectTransform toRect = to.GetComponent<RectTransform>();
+        if (@this.content.transform.Cast<Transform>().Contains(from.transform) == false) from = null;
         
+        RectTransform scrollRect = @this.GetComponent<RectTransform>();
+        RectTransform viewportRect = @this.viewport.GetComponent<RectTransform>();
+        RectTransform toRect = to.GetComponent<RectTransform>();
         Vector3 scrollPosition = @this.content.transform.localPosition;
         
-        scrollPosition.y = (-toRect.localPosition.y) - toRect.rect.height/2;
+        if (from == null)   // No old focus -> Scroll to the top
+        {
+            Debug.Log("no From");
+            
+            scrollPosition.y = (-toRect.localPosition.y) - toRect.rect.height/2;
+            // Manage scroll limit
+            float scrollLimit = @this.content.rect.height - @this.viewport.rect.height;
+            if (scrollPosition.y < 0) scrollPosition.y = 0;
+            if (scrollPosition.y > scrollLimit) scrollPosition.y = scrollLimit;
+        }
+        else if (RectTransformUtility.RectangleContainsScreenPoint(viewportRect, toRect.position, null))
+        {
+            Debug.Log("no move");
+        }
+        else
+        {
+            RectTransform fromRect = from?.GetComponent<RectTransform>();
+            scrollPosition.y += fromRect.localPosition.y - toRect.localPosition.y;
+        }
         
-        // Manage scroll limit
-        float scrollLimit = @this.content.rect.height - @this.viewport.rect.height;
-        if (scrollPosition.y < 0) scrollPosition.y = 0;
-        if (scrollPosition.y > scrollLimit) scrollPosition.y = scrollLimit;
 
 
         @this.content.transform.localPosition = scrollPosition;
