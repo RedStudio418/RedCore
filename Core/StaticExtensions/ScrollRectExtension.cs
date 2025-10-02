f4c97854e70d41b25cb354ae9b4837d856a13690
Kévin Roussel <keeevin.roussel@gmail.com>
2025-10-02 14:43:26 +0200

    Scroll Rect Extension first implementation

--- Core/StaticExtensions/ScrollRectExtension.cs
+++ Core/StaticExtensions/ScrollRectExtension.cs
@@ -6,11 +6,20 @@ public static class ScrollRectExtension
 
     public static void FocusElement(this ScrollRect @this, GameObject from, GameObject to)
     {
-        var deltaY = from.transform.position.y - to.transform.position.y;
-        @this.content.transform.position = new Vector3(
-            @this.content.transform.position.x, 
-            @this.content.transform.position.y + deltaY, 
-            @this.content.transform.position.z);
+        RectTransform fromRect = from.GetComponent<RectTransform>();
+        RectTransform toRect = to.GetComponent<RectTransform>();
+        
+        Vector3 scrollPosition = @this.content.transform.localPosition;
+        
+        scrollPosition.y = (-toRect.localPosition.y) - toRect.rect.height/2;
+        
+        // Manage scroll limit
+        float scrollLimit = @this.content.rect.height - @this.viewport.rect.height;
+        if (scrollPosition.y < 0) scrollPosition.y = 0;
+        if (scrollPosition.y > scrollLimit) scrollPosition.y = scrollLimit;
+
+
+        @this.content.transform.localPosition = scrollPosition;
     }
     
 }
